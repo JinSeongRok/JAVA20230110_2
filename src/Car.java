fcaa02c53077ef4d1fadefcb4b4bc5b28737c035
JinSeongRok <jk3858@naver.com>
2023-01-10 15:46:39 +0900

    예제연습

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,30 @@
 
 public class Car {
 	// 필드 선언
-	String model;
-	boolean start;
-	int speed;
+	String company = "현대자동차";
+	String model; 
+	String color;
+	int maxSpeed;
+	//boolean start;
+//	int speed;
 		
+	Car(){
+		this("자가용","은색", 250);
+	}
+	
+	Car(String model) {
+		this(model, "은색", 250);
+	}
+
+	Car(String model, String color) {
+		this(model, color, 250);
+	}
+
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+	    this.color = color;
+	    this.maxSpeed = maxSpeed;
+	}
+	
+	
 }
